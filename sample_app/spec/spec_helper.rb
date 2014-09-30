1459377c72e31a42683dc6b4f08459ee2188a92a
jjhlzn <jinjunhang@JINJunhangdeMac.local>
2014-09-30 09:55:37 +0800

    update

--- sample_app/spec/spec_helper.rb
+++ sample_app/spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'spork'
+require 'Capybara'
+
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
@@ -11,26 +13,25 @@ require 'rspec/autorun'
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line. ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 RSpec.configure do |config|
-# ## Mock Framework
-#
-# If you prefer to use mocha, flexmock or RR, uncomment the appropriate line: #
-# config.mock_with :mocha
-# config.mock_with :flexmock
-# config.mock_with :rr
-# Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-# If you're not using ActiveRecord, or you'd prefer not to run each of your # examples within a transaction, remove the following line or assign false # instead of true.
-  config.use_transactional_fixtures = true
-# If true, the base class of anonymous controllers will be inferred
-# automatically. This will be the default behavior in future versions of # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-# Run specs in random order to surface order dependencies. If you find an
-  99
-# order dependency and want to debug it, you can fix the order by providing # the seed, which is printed after each run.
-# --seed 1234
-  config.order = "random"
-  config.include Capybara::DSL
-end
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line: #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your # examples within a transaction, remove the following line or assign false # instead of true.
+    config.use_transactional_fixtures = true
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of # rspec-rails.
+    config.infer_base_class_for_anonymous_controllers = false
+  # Run specs in random order to surface order dependencies. If you find an
+    99
+  # order dependency and want to debug it, you can fix the order by providing # the seed, which is printed after each run.
+  # --seed 1234
+    config.order = "random"
+    config.include Capybara::DSL
 end
 Spork.each_run do
   # This code will be run each time you run your specs.
@@ -84,7 +85,7 @@ end
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'Capybara'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
