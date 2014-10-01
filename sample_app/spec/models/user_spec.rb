8f8dceac18443d68276b1a08f5bd45290159bb6d
jjhlzn <jinjunhang@hotmail.com>
2014-10-01 14:45:03 +0800

    exercise 6.5

--- sample_app/spec/models/user_spec.rb
+++ sample_app/spec/models/user_spec.rb
@@ -91,4 +91,13 @@ RSpec.describe User, :type => :model do
     it { should be_invalid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
 end
